1b7265e8bc08e964a223f2c32f08128fb579d7b3
testxxxxxxxxx <pppppmarcin@gmail.com>
2023-08-04 19:41:38 +0000

    created CommisionJob

---
+++ app/Jobs/CommisionJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Services\CommisionService;
+
+class CommisionJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(CommisionService $commisionService): void
+    {
+        for($i=0;$i<100;$i++)
+        {
+            $amountIsSaved=$commisionService->saveAmount($i);
+
+        }
+
+    }
+}
